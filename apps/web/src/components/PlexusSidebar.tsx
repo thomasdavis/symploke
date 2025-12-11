583e8bd905dd9c1e1b4c7acaba30d7681128af8e
Ajax Davis <thomasalwyndavis@gmail.com>
2025-12-12 00:28:08 +1000

    Move Run Weaves button to sidebar below plexus selector

--- apps/web/src/components/PlexusSidebar.tsx
+++ apps/web/src/components/PlexusSidebar.tsx
@@ -2,6 +2,7 @@
 
 import { PlexusSelector } from './PlexusSelector'
 import { PlexusNav } from './PlexusNav'
+import { RunWeavesButton } from './RunWeavesButton'
 import './PlexusSidebar.css'
 
 type Plexus = {
@@ -19,6 +20,9 @@ export function PlexusSidebar({ currentPlexus, userPlexuses }: PlexusSidebarProp
   return (
     <aside className="plexus-sidebar">
       <PlexusSelector currentPlexus={currentPlexus} plexuses={userPlexuses} />
+      <div className="plexus-sidebar__action">
+        <RunWeavesButton plexusId={currentPlexus.id} variant="primary" size="sm" />
+      </div>
       <PlexusNav plexusId={currentPlexus.id} />
     </aside>
   )
--- apps/web/src/components/RunWeavesButton.tsx
+++ apps/web/src/components/RunWeavesButton.tsx
@@ -125,7 +125,7 @@ export function RunWeavesButton({
       }
       return 'Running...'
     }
-    return 'Run Weave Discovery'
+    return 'Run Weaves'
   }
 
   return (
